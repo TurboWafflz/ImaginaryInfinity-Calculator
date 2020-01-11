0ffd6ef4bf7499e665aa7a803a2ab4e61ff3114e
Finian Wright <turbowafflz@gmail.com>
2020-01-10 22:49:26 -0500

    Add command for viewing the README on Linux

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from plugins.core import *
 
 import pkgutil
 import sys
+import platform
 os.system("clear")
 global cplx
 global onlineMode
@@ -22,13 +23,17 @@ def main():
 	print(Fore.BLACK + Back.WHITE + "ImaginaryInfinity Calculator v2.0")
 	print(Fore.RESET + Back.RESET + "Copyright 2019 Finian Wright")
 	print(Fore.BLUE + "https://turbowafflz.github.io/iicalc.html" + Fore.RESET)
-	print("Type 'chelp()' for help")
+	print("Type 'chelp()' for a list of commands")
+	print("Read README")
 	if(len(sys.argv)>1):
 		if(sys.argv[1]=="online"):
 			onlineMode=True
-			print(Fore.RED + "Online mode, some commands may not work and plugins cannot be added")
+			print(Fore.RED + "Online mode, plugins cannot be added")
 	else:
-		print(Fore.GREEN + "Local mode, all commands should work")
+		if(platform.system()=="Linux"):
+			print(Fore.GREEN + "Linux mode, all features should be fully supported")
+		else:
+			print(Fore.YELLOW + "Local mode, most features should be supported")
 		onlineMode=False
 	global cplx
 	ans=0
