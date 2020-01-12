9123cdc9f9f207df26481d0e27f98542c7dd2ecd
Finian Wright <turbowafflz@gmail.com>
2020-01-11 19:48:43 -0500

    Fix missing modules

--- plugins/dev.py
+++ plugins/dev.py
@@ -1,4 +1,4 @@
 def switchBranch(branch):
-    os.system("git pull https://github.com/TurboWafflz/ImaginaryInfinity-Calculator master")
+    os.system("git pull https://github.com/TurboWafflz/ImaginaryInfinity-Calculator " + branch)
     os.system("touch .start")
     exit()
