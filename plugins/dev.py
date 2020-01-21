b8b44c3f71c1b09bbfe2abdf84c2c444f0b44f11
Finian Wright <finianwright@gmail.com>
2020-01-21 17:18:31 -0500

    Make calc restart after switching branch

--- plugins/dev.py
+++ plugins/dev.py
@@ -3,6 +3,7 @@ import os
 from colorama import Fore, Back
 def switchBranch(branch):
     if branch != "master":
+        os.system("touch .start")
         print(Fore.RESET + "Warning, branches other than " + Fore.CYAN + "master " + Fore.RESET + "may be unstable and buggy. Are you sure you want to continue switching to " + Fore.CYAN + branch + Fore.RESET + "?(y/n)")
         yn=input()
         if yn=="n":
