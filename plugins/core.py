9c6ab6e8589cfc4c2b743ace30c32d0a4d6f0aba
Finian Wright <finianwright@gmail.com>
2020-02-19 14:40:43 -0500

    Start unifying style information in a class

--- plugins/core.py
+++ plugins/core.py
@@ -5,8 +5,17 @@ import platform
 from colorama import Fore
 from colorama import Back
 from colorama import Style
+import colorama
 import math
 import sys
+class style:
+	normal=colorama.Fore.RESET + colorama.Back.RESET + colorama.Style.NORMAL
+	error=colorama.Fore.RED + colorama.Back.RESET + colorama.Style.BRIGHT
+	important=colorama.Fore.MAGENTA + colorama.Back.RESET + colorama.Style.BRIGHT
+	startupmessage=colorama.Fore.YELLOW + colorama.Back.RESET + colorama.Style.NORMAL
+	prompt=colorama.Fore.GREEN + colorama.Back.RESET + colorama.Style.BRIGHT
+	link=colorama.Fore.BLUE + colorama.Back.RESET + colorama.Style.NORMAL
+	answer=colorama.Fore.GREEN + colorama.Back.RESET + colorama.Style.NORMAL
 
 #Help
 def chelp():
