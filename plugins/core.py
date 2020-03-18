8303bd1222ee6b2acb88a271b519061e6a826263
Finian Wright <turbowafflz@gmail.com>
2020-03-18 12:12:38 -0400

    Add color pallate for light terminals

--- plugins/core.py
+++ plugins/core.py
@@ -8,12 +8,23 @@ from colorama import Style
 import colorama
 import math
 import sys
-class style:
+class darkStyle:
 	normal=colorama.Fore.RESET + colorama.Back.RESET + colorama.Style.NORMAL
 	error=colorama.Fore.RED + colorama.Back.RESET + colorama.Style.BRIGHT
 	important=colorama.Fore.MAGENTA + colorama.Back.RESET + colorama.Style.BRIGHT
 	startupmessage=colorama.Fore.YELLOW + colorama.Back.RESET + colorama.Style.NORMAL
 	prompt=colorama.Fore.GREEN + colorama.Back.RESET + colorama.Style.BRIGHT
+	link=colorama.Fore.BLUE + colorama.Back.RESET + colorama.Style.NORMAL
+	answer=colorama.Fore.GREEN + colorama.Back.RESET + colorama.Style.NORMAL
+	input=colorama.Fore.CYAN + colorama.Back.RESET + colorama.Style.NORMAL
+	output=colorama.Fore.WHITE + colorama.Back.RESET + colorama.Style.NORMAL
+
+class lightStyle:
+	normal=colorama.Fore.RESET + colorama.Back.RESET + colorama.Style.NORMAL
+	error=colorama.Fore.RED + colorama.Back.RESET + colorama.Style.BRIGHT
+	important=colorama.Fore.MAGENTA + colorama.Back.RESET + colorama.Style.BRIGHT
+	startupmessage=colorama.Fore.YELLOW + colorama.Back.RESET + colorama.Style.NORMAL
+	prompt=colorama.Fore.BLACK + colorama.Back.RESET + colorama.Style.BRIGHT
 	link=colorama.Fore.BLUE + colorama.Back.RESET + colorama.Style.NORMAL
 	answer=colorama.Fore.GREEN + colorama.Back.RESET + colorama.Style.NORMAL
 	input=colorama.Fore.CYAN + colorama.Back.RESET + colorama.Style.NORMAL
