50a61cfa0584ddd8fcd15dc1d7eb4d5cee694467
Finian Wright <turbowafflz@gmail.com>
2020-06-12 11:50:51 -0400

    Improve theme security check

--- themes/__init__.py
+++ themes/__init__.py
@@ -1,16 +1,16 @@
-import os
-import inspect
-for module in os.listdir("themes"):
-	if module == '__init__.py' or module[-3:] != '.py':
-		continue
-	module=module[:-3]
-	moduleRaw=module
-	module="themes."+module
-	#print(module)
-	__import__(module, locals(), globals())
-	#functions = dir(moduleRaw)
-	#print(dir(moduleRaw))
-	#for function in functions:
-	#	__import__(module, locals(), globals(), fromlist=function)
-	#	print(module+"."+function)
-del module
+# import os
+# import inspect
+# for module in os.listdir("themes"):
+# 	if module == '__init__.py' or module[-3:] != '.py':
+# 		continue
+# 	module=module[:-3]
+# 	moduleRaw=module
+# 	module="themes."+module
+# 	#print(module)
+# 	__import__(module, locals(), globals())
+# 	#functions = dir(moduleRaw)
+# 	#print(dir(moduleRaw))
+# 	#for function in functions:
+# 	#	__import__(module, locals(), globals(), fromlist=function)
+# 	#	print(module+"."+function)
+# del module
