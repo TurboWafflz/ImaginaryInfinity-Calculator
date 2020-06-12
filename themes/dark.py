ecb69623c4502750f7d9608a91ca67da9b1d12d0
Finian Wright <turbowafflz@gmail.com>
2020-06-11 22:49:06 -0400

    Make links more readable in dark theme

--- themes/dark.py
+++ themes/dark.py
@@ -5,7 +5,7 @@ class dark:
 	important=colorama.Fore.MAGENTA + colorama.Back.RESET + colorama.Style.BRIGHT
 	startupmessage=colorama.Fore.YELLOW + colorama.Back.RESET + colorama.Style.NORMAL
 	prompt=colorama.Fore.GREEN + colorama.Back.RESET + colorama.Style.BRIGHT
-	link=colorama.Fore.BLUE + colorama.Back.RESET + colorama.Style.NORMAL
+	link=colorama.Fore.BLUE + colorama.Back.RESET + colorama.Style.BRIGHT
 	answer=colorama.Fore.GREEN + colorama.Back.RESET + colorama.Style.NORMAL
 	input=colorama.Fore.CYAN + colorama.Back.RESET + colorama.Style.NORMAL
 	output=colorama.Fore.WHITE + colorama.Back.RESET + colorama.Style.NORMAL
