06ef12ea2c3e66c059da730ac69ba800a199bc59
Finian Wright <turbowafflz@gmail.com>
2020-06-22 17:52:10 -0400

    Allow bgm to be disabled

--- plugins/bgm.py
+++ plugins/bgm.py
@@ -1,3 +1,11 @@
-import simpleaudio as sa
-bgm = sa.WaveObject.from_wave_file(".bgm.wav")
-bgmPlay = bgm.play()
+import configparser
+builtin = True;
+config = configparser.ConfigParser()
+config.read("config.ini")
+try:
+	if config["startup"]["bgm"] == "true":
+		import simpleaudio as sa
+		bgm = sa.WaveObject.from_wave_file(".bgm.wav")
+		bgmPlay = bgm.play()
+except:
+	pass
