7ddff4e083ce111e48023a4feab3dcb83f78d4f4
Tabulate <tabulatejarl8@gmail.com>
2020-06-24 16:28:54 -0400

    Add files via upload

---
+++ plugins/store.py
@@ -0,0 +1,76 @@
+import requests
+from dialog import Dialog
+import configparser
+from fuzzywuzzy import fuzz
+config = configparser.ConfigParser()
+config.read("test.ini")
+
+def pluginpage(plugin):
+	d = Dialog(dialog="dialog")
+	d.add_persistent_args(["--ok-label", "Download", "--title", plugin])
+	x = d.msgbox(config[plugin]["longdesc"] + "\n\nRating: " + config[plugin]["rating"] + "/5", height=None, width=None, extra_button=True, extra_label="Back")
+	if x == d.OK:
+		download(config[plugin]["download"], config[plugin]["filename"])
+
+
+def search():
+	d = Dialog(dialog="dialog")
+	x = d.inputbox("Search", height=None, width=None, init="")
+	if x[0] == d.OK:
+		choices = []
+		for key in config.sections():
+			if fuzz.partial_ratio(x[1].lower(), key.lower()) >= 70:
+				choices.append((key, config[key]["shortdesc"]))
+			if fuzz.partial_ratio(x[1].lower(), config[key]["longdesc"].lower()) >= 70:
+				if not (key, config[key]["shortdesc"]) in choices:
+					choices.append((key, config[key]["shortdesc"]))
+		text = " "
+		if len(choices) == 0:
+			choices.append(("", ""))
+			text="No Results"
+		x = d.menu(text, height=None, width=None, menu_height=None, choices=choices, cancel_label="Back")
+		if x[0] == d.OK:
+			pluginpage(x[1])
+
+def download(link, file_name):
+	d = Dialog(dialog="dialog")
+	d.add_persistent_args(["--title", "Downloading " + file_name])
+	d.gauge_start(text="", height=None, width=None, percent=0)
+	with open(file_name, "wb") as f:
+		response = requests.get(link, stream=True)
+		total_length = response.headers.get('content-length')
+
+		if total_length is None: # no content length header
+			f.write(response.content)
+		else:
+			dl = 0
+			total_length = int(total_length)
+			olddone=0
+			for data in response.iter_content(chunk_size=4096):
+				dl += len(data)
+				f.write(data)
+				done = int(100 * dl / total_length)
+				if done > 100:
+					done = 100
+				if olddone != done:
+					olddone = done
+						
+					d.gauge_update(done)
+	d.gauge_stop()
+	d.msgbox("Successfully downloaded " + file_name, height=None, width=None)
+	
+def store():
+	d = Dialog(dialog="dialog")
+	d.add_persistent_args(["--title", "Browse", "--cancel-label", "Quit"])
+	choices = [("Search", "Search for plugins")]
+	for key in config.sections():
+		choices.append((key, config[key]["shortdesc"]))
+	while True:
+		mainmenu = d.menu("", height=None, width=None, menu_height=None, choices=choices)
+		if mainmenu[0] == d.CANCEL:
+			break
+		elif mainmenu[1] == "Search":
+			search()
+		else:
+			pluginpage(mainmenu[1])
+store()
