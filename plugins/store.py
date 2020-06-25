436110dd38ea061209e0309e3bc3254ddd12d828
TabulateJarl8 <tabulatejarl8@gmail.com>
2020-06-24 22:34:19 -0400

    fix plugin updating in store

--- plugins/store.py
+++ plugins/store.py
@@ -4,6 +4,8 @@ import configparser
 from fuzzywuzzy import fuzz
 import os
 config = configparser.ConfigParser()
+pluginconfig = configparser.ConfigParser()
+pluginconfig.read("plugins/.plugins.ini")
 
 def reloadPluginList():
 	file_name = "plugins/.index.ini"
@@ -29,17 +31,16 @@ def reloadPluginList():
 					done = 100
 				if olddone != done:
 					olddone = done
-						
+
 					d.gauge_update(done)
 	d.gauge_stop()
-	d.msgbox("Successfully reloaded plugin list", height=None, width=None)
 
 reloadPluginList()
 config.read("plugins/.index.ini")
 
 def uninstall(filename):
 	d = Dialog(dialog="dialog")
-	if d.yesno("Would you like to uninstall " + filename + "?", height=0, width=0) == d.OK:	
+	if d.yesno("Would you like to uninstall " + filename + "?", height=0, width=0) == d.OK:
 		os.remove("plugins/" + filename)
 		d.msgbox(filename + " has been uninstalled.", width=None, height=None)
 
@@ -47,7 +48,7 @@ def ratePlugin(plugin):
 	d = Dialog(dialog="dialog")
 	requests.post("https://turbowafflz.azurewebsites.net/iicalc/plugins/rate", {"plugin":plugin, "rating":d.rangebox("Rate " + plugin, height=0, width=0, min=1, max=5, init=5)[1]})
 
-def download(link, file_name):
+def download(link, file_name, plugin_name):
 	d = Dialog(dialog="dialog")
 	d.add_persistent_args(["--title", "Downloading " + file_name])
 	d.gauge_start(text="", height=None, width=None, percent=0)
@@ -69,28 +70,40 @@ def download(link, file_name):
 					done = 100
 				if olddone != done:
 					olddone = done
-						
+
 					d.gauge_update(done)
 	d.gauge_stop()
-	d.msgbox("Successfully downloaded " + file_name, height=None, width=None)	
+	try:
+		pluginconfig.add_section(plugin_name)
+	except:
+		pass
+	pluginconfig[plugin_name]["lastupdate"] = config[plugin_name]["lastUpdate"]
+	with open("plugins/.plugins.ini", "r+") as f:
+		pluginconfig.write(f)
+	d.msgbox("Successfully downloaded " + file_name, height=None, width=None)
 
 def pluginpage(plugin):
 	d = Dialog(dialog="dialog")
 	d.add_persistent_args(["--yes-label", "Download", "--ok-label", "Download", "--title", plugin])
 	x = []
 	if os.path.isfile("plugins/" + config[plugin]["filename"]):
-		if os.stat("plugins/" + config[plugin]["filename"]).st_size == len(requests.get(config[plugin]["download"], stream=True).content):		
-			x.append(d.yesno(config[plugin]["longdesc"] + "\n\nRating: " + config[plugin]["rating"] + "/5", height=0, width=0, no_label="Back", cancel_label="Back", extra_button=True, extra_label="Rate Plugin", yes_label="Uninstall", ok_label="Uninstall"))
-			x.append("uninstall")
-		else:
-			 x.append(d.yesno(config[plugin]["longdesc"] + "\n\nRating: " + config[plugin]["rating"] + "/5", height=0, width=0, no_label="Back", cancel_label="Back", yes_label="Update", ok_label="Update", help_button=True, help_label="Uninstall"))
-			 x.append("update")
+		try:
+			if pluginconfig[plugin]["lastupdate"] == config[plugin]["lastUpdate"]:
+				x.append(d.yesno(config[plugin]["longdesc"] + "\n\nRating: " + config[plugin]["rating"] + "/5", height=0, width=0, no_label="Back", cancel_label="Back", extra_button=True, extra_label="Rate Plugin", yes_label="Uninstall", ok_label="Uninstall"))
+				x.append("uninstall")
+			else:
+				 x.append(d.yesno(config[plugin]["longdesc"] + "\n\nRating: " + config[plugin]["rating"] + "/5", height=0, width=0, no_label="Back", cancel_label="Back", yes_label="Update", ok_label="Update", help_button=True, help_label="Uninstall"))
+				 x.append("update")
+		except KeyError:
+			print(pluginconfig[plugin])
+			x.append(d.yesno(config[plugin]["longdesc"] + "\n\nRating: " + config[plugin]["rating"] + "/5", height=0, width=0, no_label="Back", cancel_label="Back"))
+			x.append("download")
 	else:
 		x.append(d.yesno(config[plugin]["longdesc"] + "\n\nRating: " + config[plugin]["rating"] + "/5", height=0, width=0, no_label="Back", cancel_label="Back"))
 		x.append("download")
 	if x[0] == d.OK:
 		if x[1] == "download" or x[1] == "update":
-			download(config[plugin]["download"], "plugins/" + config[plugin]["filename"])
+			download(config[plugin]["download"], "plugins/" + config[plugin]["filename"], plugin)
 		elif x[1] == "uninstall":
 			uninstall(config[plugin]["filename"])
 	elif x[0] == d.EXTRA:
@@ -116,7 +129,7 @@ def search():
 		x = d.menu(text, height=None, width=None, menu_height=None, choices=choices, cancel_label="Back")
 		if x[0] == d.OK:
 			pluginpage(x[1])
-	
+
 def store():
 	d = Dialog(dialog="dialog")
 	d.add_persistent_args(["--title", "Browse", "--cancel-label", "Quit"])
@@ -131,3 +144,4 @@ def store():
 			search()
 		else:
 			pluginpage(mainmenu[1])
+store()
