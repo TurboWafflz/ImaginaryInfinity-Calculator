9e6731bc51ada14c6e8bebf701ab3f9ae9f5858c
Finian Wright <turbowafflz@gmail.com>
2020-08-13 17:55:09 -0400

    Add manual installation for unsupported OSs

--- installer.sh
+++ installer.sh
@@ -15,13 +15,13 @@ then
 		exit
 	fi
 	systemPath="/usr/share/iicalc/"
-	userPath="$HOME/.iicalc"
 	binPath="/usr/bin/"
 	config=".installer/configDefaults/unix.ini"
-	launcher=".installer/launchers/linux.sh"
+	launcher=".installer/launchers/unix.sh"
 	iconPath="/usr/share/icons"
 	desktopFilePath="/usr/share/applications"
 	desktopFile=".installer/desktopFiles/iicalc.desktop"
+	installDesktopFile="true"
 elif [ `uname` == "Darwin" ]
 then
 	if [ `whoami` != "root" ]
@@ -37,21 +37,40 @@ then
 		exit
 	fi
 	systemPath="/usr/share/iicalc/"
-	userPath="$HOME/.iicalc"
 	binPath="/usr/bin/"
-	config=".installer/configDefaults/linux.ini"
+	config=".installer/configDefaults/unix.ini"
 	launcher=".installer/launchers/unix.sh"
 	iconPath="/usr/share/iicalc/"
 	desktopFilePath="/Applications/"
 	desktopFile=".installer/desktopFiles/ImaginaryInfinity_Calculator"
+	installDesktopFile="true"
 else
-	echo "The installer does not currently support your operating system"
-	exit
+	echo "The installer does not currently support your operating system. You can install the calculator by manually specifying the required paths, however this is only recommended for experienced users."
+	echo "Would you like to start manual installation (y/N)?"
+	read yn
+	if [ $yn != "y" ]
+	then
+		exit
+	fi
+	echo "Where should plugins and themes that are installed system wide be stored? (Ex. /usr/share/iicalc/)"
+	read systemPath
+	echo "Where should executable files be stored? (Ex. /usr/bin/)"
+	read binPath
+	echo "Where should icons be stored?"
+	read iconPath
+	installDesktopFile="false"
+	cp .installer/launchers/unix.sh .installer/launchers/custom.sh
+	launcher=".installer/launchers/custom.sh"
+	cat "$launcher" | sed "s'systemPath=\"/usr/share/iicalc/\"'systemPath=$systemPath'" > $launcher
+
 fi
 cd $DIR
 echo "Installing launcher..."
 cp $launcher "$binPath/iicalc"
-cp -r $desktopFile $desktopFilePath
+if [ $installDesktopFile == "true" ]
+then
+	cp -r $desktopFile $desktopFilePath
+fi
 chmod +x "$binPath/iicalc"
 echo "Installing main Python script.."
 cp main.py "$binPath/iicalc.py"
