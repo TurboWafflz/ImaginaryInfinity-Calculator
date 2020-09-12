4ab6074cb2d44e63b090587a680d121ec4d1d881
Finian Wright <turbowafflz@gmail.com>
2020-09-11 22:43:25 -0400

    Launchers now offer to install requirements on first startup

--- .installer/launchers/unix.sh
+++ .installer/launchers/unix.sh
@@ -3,10 +3,20 @@ systemPath="/usr/share/iicalc/"
 userPath="$HOME/.iicalc"
 if [ ! -d "$userPath" ]
 then
+	clear
+	echo "First time setup"
+	echo ""
+	echo "Would you like to attempt to install required Python modules? (Y/n)"
+	read yn
+	if [ "$yn" != "n" ]
+	then
+		python3 -m pip install -r "$systemPath/requirements.txt"
+	fi
 	echo "Creating user folder..."
 	mkdir $userPath
 	mkdir "$userPath/plugins"
 	mkdir "$userPath/themes"
 	cp "$systemPath/config.ini" "$userPath/config.ini"
+	clear
 fi
 python3 $systemPath/iicalc.py
