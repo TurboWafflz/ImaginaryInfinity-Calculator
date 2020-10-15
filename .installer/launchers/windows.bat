a37bbdb147982de0a1d04023c191bce605b40dfd
TabulateJarl8 <tabulatejarl8@gmail.com>
2020-10-15 19:11:08 -0400

    windows testing

--- .installer/launchers/windows.bat
+++ .installer/launchers/windows.bat
@@ -1,5 +1,22 @@
-cd "C:\Program Files (x86)\iicalc\"
-py iicalc.py
 @echo off
+
+SET systemPath=C:\Program Files(x86)\iicalc\
+SET userPath="%userprofile%\.iicalc"
+IF NOT EXIST %userPath% (
+	cls
+	echo "First time setup"
+	echo
+	SET /p inst="Would you like to attempt to install required Python modules? (Y\n)"
+	IF NOT %inst%==n (
+		py -m pip install -r "%systemPath%\requirements.txt"
+	)
+	echo "Creating user folder..."
+	mkdir %userPath%
+	mkdir "%userPath%\plugins"
+	mkdir "%userPath%\themes"
+	COPY  "%systemPath%\config.ini" "%userPath%\config.ini"
+	cls
+)
+py %systemPath%\iicalc.py
 cmd /k
 @echo on
