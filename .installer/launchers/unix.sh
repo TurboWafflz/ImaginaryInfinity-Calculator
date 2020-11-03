1941309c4841e7581215d9370fd44093ed2104b1
TabulateJarl8 <tabulatejarl8@gmail.com>
2020-11-03 16:53:06 -0500

    install python-Levenshtein if gcc is installed

--- .installer/launchers/unix.sh
+++ .installer/launchers/unix.sh
@@ -11,6 +11,11 @@ then
 	if [ "$yn" != "n" ]
 	then
 		python3 -m pip install -r "$systemPath/requirements.txt"
+		gcc -v 1> /dev/null 2> /dev/null
+		if [ "$?" == "0" ]
+		then
+			python3 -m pip install python-Levenshtein
+		fi
 	fi
 	echo "Creating user folder..."
 	mkdir $userPath
