8b7aad673dfa175dd021d703b465901554b99750
TabulateJarl8 <tabulatejarl8@gmail.com>
2020-11-07 01:20:46 -0500

    launcher

--- .installer/launchers/windows.bat
+++ .installer/launchers/windows.bat
@@ -1,17 +1,13 @@
 @echo off
 
-SET systemPath=C:\Program Files(x86)\iicalc\
-SET userPath="%userprofile%\.iicalc"
-IF EXIST %userPath% (
-	py %systemPath%\iicalc.py
-	cmd /k
-	@echo on
-) ELSE (
+SET systemPath="C:\Program Files(x86)\iicalc\"
+SET userPath="%userprofile%\.iicalc\"
+IF NOT EXIST %userPath% (
 	cls
 	echo "First time setup"
 	echo
 	SET /p inst="Would you like to attempt to install required Python modules? (Y\n)"
-	IF NOT %inst%==n (
+	IF NOT "%inst%"=="n" (
 		py -m pip install -r "%systemPath%\requirements.txt"
 	)
 	echo "Creating user folder..."
@@ -21,3 +17,5 @@ IF EXIST %userPath% (
 	COPY  "%systemPath%\config.ini" "%userPath%\config.ini"
 	cls
 )
+py %systemPath%\iicalc.py
+@echo on
