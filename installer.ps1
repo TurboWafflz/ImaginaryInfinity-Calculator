15722974433569f2da83fa638b8a5572fb9323b3
TabulateJarl8 <tabulatejarl8@gmail.com>
2020-11-07 00:30:56 -0500

    features

--- installer.ps1
+++ installer.ps1
@@ -27,9 +27,25 @@ echo "Installing main Python script.."
 cp main.py "$systemPath\iicalc.py" -force
 cp requirements.txt "$systemPath" -force
 cp messages.txt "$systemPath" -force
+cp system\version.txt "$systemPath" -force
+cp .\README-online "$systemPath" -force
 cp $config "$systemPath\config.ini" -force
+
 echo "Installing icons..."
 cp ".\iicalc.ico" "$systemPath\iicalc.ico"
 Invoke-Expression "C:\Windows\System32\Cscript.exe .\scripts.vbs startmenu //nologo"
+
+py -V > $null
+if($? -eq $false){
+	$yn = Read-Host "Python is not installed. You must install Python to run the calculator Install it now? [Y/n] "
+	if($yn -ne "n"){
+		echo "Downloading installer..."
+		[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+		Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.9.0/python-3.9.0-amd64.exe" -OutFile ".\python-3.9.0.exe"
+		echo "Installing Python, this make take a bit..."
+		.\python-3.9.0.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
+	}
+}
+
 echo "Installing Python modules..."
 py -m pip install -r requirements.txt
