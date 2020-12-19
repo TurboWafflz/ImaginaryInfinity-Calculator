8efc41e1aa6074bc85b636ef1c31e1c7fc2f2207
TabulateJarl8 <tabulatejarl8@gmail.com>
2020-12-18 23:21:50 -0500

    finish support for custom URI

--- .installer/launchers/netbsd.sh
+++ .installer/launchers/netbsd.sh
@@ -21,4 +21,26 @@ then
 	clear
 fi
 
-$python $systemPath/iicalc.py $@
+# Get URI Path if specified
+uri="none"
+for arg in $@; do
+	if grep -q iicalc:// <<< "$arg"; then
+		uri=$(echo $arg | sed -e 's/^.*iicalc:\/\///g')
+		break
+	fi
+done
+if [ "$uri" != "none" ]; then
+	IFS='/' read -r -a uriarray <<< "$uri"
+	if [ "${uriarray[0]}" == "plugin" ]; then
+		plugin="${uriarray[1]}"
+		if [ "${uriarray[2]}" == "dialog" ]; then
+			$python $systemPath/iicalc.py --viewstoreplugin $plugin
+		elif [ "${uriarray[2]}" == "ascii" ]; then
+			$python $systemPath/iicalc.py --installpmplugin $plugin
+		else
+			$python $systemPath/iicalc.py $@
+		fi
+	fi
+else
+	$python $systemPath/iicalc.py $@
+fi
